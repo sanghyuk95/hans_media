1c102d28fd40b787c283e6773d548abb116230b1
burny-laptop <shskse5@naver.com>
2023-08-11 23:56:23 +0900

    1차 완성

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 ## page 링크
 
-https://sanghyuk95.github.io/hans_media/
+https://sanghyuk95.github.io/hans_media/page1.html
+https://sanghyuk95.github.io/hans_media/page2.html
+https://sanghyuk95.github.io/hans_media/page3.html
+https://sanghyuk95.github.io/hans_media/page4.html
+https://sanghyuk95.github.io/hans_media/page5.html
--- js/script.js
+++ js/script.js
@@ -1,78 +1,87 @@
-// 사이드바
-const yearTitle = document.querySelectorAll(".year-title");
-const monthTitle = document.querySelectorAll(".month-title");
-const day = document.querySelectorAll(".day");
+window.onload = function () {
+  // 사이드바
+  const yearTitle = document.querySelectorAll(".year-title");
+  const monthTitle = document.querySelectorAll(".month-title");
+  const day = document.querySelectorAll(".day");
 
-yearTitle.forEach((item) => {
-  item.addEventListener("click", () => {
-    item.classList.toggle("active", !item.classList.contains("active"));
-  });
-});
-monthTitle.forEach((item) => {
-  item.addEventListener("click", () => {
-    item.classList.toggle("active", !item.classList.contains("active"));
-  });
-});
-
-day.forEach((item, idx) => {
-  item.addEventListener("click", () => {
-    day.forEach((a, i) => {
-      a.classList.toggle("active", idx === i);
+  yearTitle.forEach((item) => {
+    item.addEventListener("click", () => {
+      item.classList.toggle("active", !item.classList.contains("active"));
     });
   });
-});
-
-// 전체체크
-const allCheck = document.querySelector(".all-check");
-const checkbox = document.querySelectorAll('input[type="checkbox"]');
-console.log(checkbox);
-
-allCheck.addEventListener("click", () => {
-  checkbox.forEach((item) => {
-    if (allCheck.checked) {
-      item.checked = true;
-    } else {
-      item.checked = false;
-    }
+  monthTitle.forEach((item) => {
+    item.addEventListener("click", () => {
+      item.classList.toggle("active", !item.classList.contains("active"));
+    });
   });
-});
 
-// 매니저버튼
-const managerImg = document.querySelector(".manager-img ");
-const managerOpen = document.querySelector(".manager-open");
-const managerClose = document.querySelector(".manager-close");
+  day.forEach((item, idx) => {
+    item.addEventListener("click", () => {
+      day.forEach((a, i) => {
+        a.classList.toggle("active", idx === i);
+      });
+    });
+  });
 
-managerImg.addEventListener("click", () => {
-  managerImg.classList.remove("active");
-  managerOpen.classList.add("active");
-});
-managerClose.addEventListener("click", () => {
-  managerImg.classList.add("active");
-  managerOpen.classList.remove("active");
-});
+  // 전체체크
+  const allCheck = document.querySelector(".all-check");
+  const checkbox = document.querySelectorAll('input[type="checkbox"]');
+  console.log(checkbox);
 
-//헤더메뉴
-const headerMenu = document.querySelector(".header-menu");
-const headerMenuImg = document.querySelector(".header-menu > img");
-const sidebar = document.querySelector(".sidebar");
-const mainBtns = document.querySelector(".main-btns");
-const search = document.querySelector(".header-search");
+  if (allCheck) {
+    allCheck.addEventListener("click", () => {
+      checkbox.forEach((item) => {
+        if (allCheck.checked) {
+          item.checked = true;
+        } else {
+          item.checked = false;
+        }
+      });
+    });
+  }
+  // 매니저버튼
+  const managerImg = document.querySelector(".manager-img ");
+  const managerOpen = document.querySelector(".manager-open");
+  const managerClose = document.querySelector(".manager-close");
+  if (managerImg) {
+    managerImg.addEventListener("click", () => {
+      managerImg.classList.remove("active");
+      managerOpen.classList.add("active");
+    });
+  }
+  if (managerClose) {
+    managerClose.addEventListener("click", () => {
+      managerImg.classList.add("active");
+      managerOpen.classList.remove("active");
+    });
+  }
 
-headerMenu.addEventListener("click", () => {
-  headerMenu.classList.toggle("active", !headerMenu.classList.contains("active"));
-  sidebar.classList.toggle("active", !sidebar.classList.contains("active"));
-  mainBtns.classList.toggle("active", !mainBtns.classList.contains("active"));
-  search.classList.toggle("active", !search.classList.contains("active"));
-});
+  //헤더메뉴
+  const headerMenu = document.querySelector(".header-menu");
+  const headerMenuImg = document.querySelector(".header-menu > img");
+  const sidebar = document.querySelector(".sidebar");
+  const mainBtns = document.querySelector(".main-btns");
+  const search = document.querySelector(".header-search");
 
-// 팝업
-const popupClose = document.querySelector(".popup-close");
-const popup = document.querySelector(".popup");
-const popupOpen = document.querySelector(".main-btns-move");
+  headerMenu.addEventListener("click", () => {
+    headerMenu.classList.toggle("active", !headerMenu.classList.contains("active"));
+    sidebar.classList.toggle("active", !sidebar.classList.contains("active"));
+    if (mainBtns) {
+      mainBtns.classList.toggle("active", !mainBtns.classList.contains("active"));
+    }
+    search.classList.toggle("active", !search.classList.contains("active"));
+  });
 
-popupOpen.addEventListener("click", () => {
-  popup.classList.add("active");
-});
-popupClose.addEventListener("click", () => {
-  popup.classList.remove("active");
-});
+  // 팝업
+  const popupClose = document.querySelector(".popup-close");
+  const popup = document.querySelector(".popup");
+  const popupOpen = document.querySelector(".main-btns-move");
+  if (popup) {
+    popupOpen.addEventListener("click", () => {
+      popup.classList.add("active");
+    });
+    popupClose.addEventListener("click", () => {
+      popup.classList.remove("active");
+    });
+  }
+};
